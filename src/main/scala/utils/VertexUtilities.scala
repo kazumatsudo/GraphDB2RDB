485f49dbc96ad4d16f366aa52359fd75cd956a1b
kazumatsudo <kazuma.matsudo@edandit.co.jp>
2024-01-06 21:23:40 +0900

    add: VertexUtilities#getAllVerticesList

--- src/main/scala/utils/VertexUtilities.scala
+++ src/main/scala/utils/VertexUtilities.scala
@@ -1,6 +1,6 @@
 package utils
 
-import gremlin.scala.GremlinScala
+import gremlin.scala.{GremlinScala, Vertex}
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource
 
 final case class VertexUtilities(g: GraphTraversalSource) {
@@ -10,4 +10,17 @@ final case class VertexUtilities(g: GraphTraversalSource) {
    * @return the number of all vertices
    */
   def countAll: Long = GremlinScala(g.V()).count().head()
+
+  /** get Vertices List
+   *
+   * @param start The position to start retrieving Vertices from (0-based index).
+   * @param count The number of Vertices to retrieve.
+   * @return A list of Vertices based on the specified pagination parameters.
+   */
+  def getVerticesList(start: Int, count: Int): Seq[Vertex] = {
+    require(start >= 0, "start must be positive.")
+    require(count >= 0, "count must be positive.")
+
+    GremlinScala(g.V()).range(start, start + count).toList()
+  }
 }
--- src/test/scala/utils/VertexUtilitiesSpec.scala
+++ src/test/scala/utils/VertexUtilitiesSpec.scala
@@ -1,5 +1,6 @@
 package utils
 
+import gremlin.scala.GremlinScala
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerFactory
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
@@ -7,9 +8,35 @@ import org.scalatest.matchers.should.Matchers
 class VertexUtilitiesSpec extends AnyFunSpec with Matchers {
   describe("countAll") {
     it ("get the number of all vertices") {
-      val graph = TinkerFactory.createModern()
-      val vertexUtilities = VertexUtilities(graph.traversal())
+      val graph = TinkerFactory.createModern().traversal()
+      val vertexUtilities = VertexUtilities(graph)
       vertexUtilities.countAll shouldBe 6
+    }
+  }
+
+  describe("getVerticesList") {
+    describe("require"){
+      it ("start must be positive.") {
+        val graph = TinkerFactory.createModern().traversal()
+        val vertexUtilities = VertexUtilities(graph)
+        intercept[IllegalArgumentException] {
+          vertexUtilities.getVerticesList(-1, 0)
+        }
+      }
+
+      it ("count must be positive.") {
+        val graph = TinkerFactory.createModern().traversal()
+        val vertexUtilities = VertexUtilities(graph)
+        intercept[IllegalArgumentException] {
+          vertexUtilities.getVerticesList(0, -1)
+        }
+      }
+    }
+
+    it ("get the number of all vertices") {
+      val graph = TinkerFactory.createModern().traversal()
+      val vertexUtilities = VertexUtilities(graph)
+      vertexUtilities.getVerticesList(0, 1) shouldBe Seq(GremlinScala(graph.V()).head())
     }
   }
 }
