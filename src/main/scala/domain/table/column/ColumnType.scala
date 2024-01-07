ca25c8c30bb70dc55d078868b7823c30f12c7db1
kazumatsudo <kazuma.matsudo@edandit.co.jp>
2024-01-07 13:17:37 +0900

    add: for ColumnType, support for Long

--- src/main/scala/domain/table/column/ColumnType.scala
+++ src/main/scala/domain/table/column/ColumnType.scala
@@ -6,6 +6,7 @@ sealed trait ColumnType
 case object ColumnTypeBoolean extends ColumnType
 
 case class ColumnTypeInt(private val length: ColumnLength) extends ColumnType
+case class ColumnTypeLong(private val length: ColumnLength) extends ColumnType
 case class ColumnTypeDouble(private val length: ColumnLength) extends ColumnType
 case class ColumnTypeString(private val length: ColumnLength) extends ColumnType
 case object ColumnTypeUnknown extends ColumnType
@@ -15,6 +16,7 @@ object ColumnType {
   def apply(value: Any): ColumnType = value match {
     case valueString: String => ColumnTypeString(ColumnLength(valueString.length))
     case valueInt: Int => ColumnTypeInt(ColumnLength(valueInt.toString.length))
+    case valueLong: Long => ColumnTypeLong(ColumnLength(valueLong.toString.length))
     case valueDouble: Double => ColumnTypeDouble(ColumnLength(valueDouble.toString.replaceAll("0*$", "").length))
     case _: Boolean => ColumnTypeBoolean
     case _ => ColumnTypeUnknown // TODO: classify the type in detail
@@ -32,6 +34,7 @@ object ColumnType {
       // ColumnTypeBoolean
       case (ColumnTypeBoolean, ColumnTypeBoolean) => ColumnTypeBoolean
       case (ColumnTypeBoolean, ColumnTypeInt(length)) =>ColumnTypeInt(length.max(5)) // 5 = false.toString
+      case (ColumnTypeBoolean, ColumnTypeLong(length)) =>ColumnTypeLong(length.max(5)) // 5 = false.toString
       case (ColumnTypeBoolean, ColumnTypeDouble(length)) => ColumnTypeDouble(length.max(5)) // 5 = false.toString
       case (ColumnTypeBoolean, ColumnTypeString(length)) => ColumnTypeString(length.max(5)) // 5 = false.toString
       case (ColumnTypeBoolean, ColumnTypeUnknown) => ColumnTypeUnknown
@@ -39,13 +42,23 @@ object ColumnType {
       // ColumnTypeInt
       case (ColumnTypeInt(_), ColumnTypeBoolean) => merge(b, a)
       case (ColumnTypeInt(alength), ColumnTypeInt(blength)) => ColumnTypeInt(alength.max(blength))
+      case (ColumnTypeInt(alength), ColumnTypeLong(blength)) => ColumnTypeLong(alength.max(blength))
       case (ColumnTypeInt(alength), ColumnTypeDouble(blength)) => ColumnTypeDouble(alength.max(blength))
       case (ColumnTypeInt(alength), ColumnTypeString(blength)) => ColumnTypeString(alength.max(blength))
       case (ColumnTypeInt(_), ColumnTypeUnknown) => ColumnTypeUnknown
 
+      // ColumnTypeLong
+      case (ColumnTypeLong(_), ColumnTypeBoolean) => merge(b, a)
+      case (ColumnTypeLong(_), ColumnTypeInt(_)) => merge(b, a)
+      case (ColumnTypeLong(alength), ColumnTypeLong(blength)) => ColumnTypeLong(alength.max(blength))
+      case (ColumnTypeLong(alength), ColumnTypeDouble(blength)) => ColumnTypeDouble(alength.max(blength))
+      case (ColumnTypeLong(alength), ColumnTypeString(blength)) => ColumnTypeString(alength.max(blength))
+      case (ColumnTypeLong(_), ColumnTypeUnknown) => ColumnTypeUnknown
+
       // ColumnTypeDouble
       case (ColumnTypeDouble(_), ColumnTypeBoolean) => merge(b, a)
       case (ColumnTypeDouble(_), ColumnTypeInt(_)) => merge(b, a)
+      case (ColumnTypeDouble(_), ColumnTypeLong(_)) => merge(b, a)
       case (ColumnTypeDouble(alength), ColumnTypeDouble(blength)) => ColumnTypeDouble(alength.max(blength))
       case (ColumnTypeDouble(alength), ColumnTypeString(blength)) => ColumnTypeString(alength.max(blength))
       case (ColumnTypeDouble(_), ColumnTypeUnknown) => ColumnTypeUnknown
@@ -53,6 +66,7 @@ object ColumnType {
       // ColumnTypeString
       case (ColumnTypeString(_), ColumnTypeBoolean) => merge(b, a)
       case (ColumnTypeString(_), ColumnTypeInt(_)) => merge(b, a)
+      case (ColumnTypeString(_), ColumnTypeLong(_)) => merge(b, a)
       case (ColumnTypeString(_), ColumnTypeDouble(_)) => merge(b, a)
       case (ColumnTypeString(alength), ColumnTypeString(blength)) => ColumnTypeString(alength.max(blength))
       case (ColumnTypeString(_), ColumnTypeUnknown) => ColumnTypeUnknown
@@ -60,6 +74,7 @@ object ColumnType {
       // ColumnTypeUnknown
       case (ColumnTypeUnknown, ColumnTypeBoolean) => merge(b, a)
       case (ColumnTypeUnknown, ColumnTypeInt(_)) => merge(b, a)
+      case (ColumnTypeUnknown, ColumnTypeLong(_)) => merge(b, a)
       case (ColumnTypeUnknown, ColumnTypeDouble(_)) => merge(b, a)
       case (ColumnTypeUnknown, ColumnTypeString(_)) => merge(b, a)
       case (ColumnTypeUnknown, ColumnTypeUnknown) => ColumnTypeUnknown
--- src/test/scala/domain/table/column/ColumnTypeSpec.scala
+++ src/test/scala/domain/table/column/ColumnTypeSpec.scala
@@ -9,6 +9,7 @@ class ColumnTypeSpec extends AnyFunSpec with Matchers {
       ColumnType.apply(false) shouldBe ColumnTypeBoolean
       ColumnType.apply("string") shouldBe ColumnTypeString(ColumnLength(6))
       ColumnType.apply(1) shouldBe ColumnTypeInt(ColumnLength(1))
+      ColumnType.apply(1.toLong) shouldBe ColumnTypeLong(ColumnLength(1))
       ColumnType.apply(1.1) shouldBe ColumnTypeDouble(ColumnLength(3))
       ColumnType.apply(Seq.empty) shouldBe ColumnTypeUnknown
     }
@@ -19,6 +20,8 @@ class ColumnTypeSpec extends AnyFunSpec with Matchers {
       val columnTypeBoolean = ColumnTypeBoolean
       val columnTypeIntSmall = ColumnTypeInt(ColumnLength(1))
       val columnTypeIntBig = ColumnTypeInt(ColumnLength(2))
+      val columnTypeLongSmall = ColumnTypeLong(ColumnLength(1))
+      val columnTypeLongBig = ColumnTypeLong(ColumnLength(2))
       val columnTypeDoubleSmall = ColumnTypeDouble(ColumnLength(1))
       val columnTypeDoubleBig = ColumnTypeDouble(ColumnLength(2))
       val columnTypeStringSmall = ColumnTypeString(ColumnLength(1))
@@ -28,6 +31,7 @@ class ColumnTypeSpec extends AnyFunSpec with Matchers {
       // ColumnTypeBoolean
       ColumnType.merge(columnTypeBoolean, columnTypeBoolean) shouldBe columnTypeBoolean
       ColumnType.merge(columnTypeBoolean, columnTypeIntBig) shouldBe ColumnTypeInt(ColumnLength(5))
+      ColumnType.merge(columnTypeBoolean, columnTypeLongBig) shouldBe ColumnTypeLong(ColumnLength(5))
       ColumnType.merge(columnTypeBoolean, columnTypeDoubleBig) shouldBe ColumnTypeDouble(ColumnLength(5))
       ColumnType.merge(columnTypeBoolean, columnTypeStringBig) shouldBe ColumnTypeString(ColumnLength(5))
       ColumnType.merge(columnTypeBoolean, columnTypeUnknown) shouldBe columnTypeUnknown
@@ -35,13 +39,23 @@ class ColumnTypeSpec extends AnyFunSpec with Matchers {
       // ColumnTypeInt
       ColumnType.merge(columnTypeIntSmall, columnTypeBoolean) shouldBe ColumnTypeInt(ColumnLength(5))
       ColumnType.merge(columnTypeIntSmall, columnTypeIntBig) shouldBe columnTypeIntBig
+      ColumnType.merge(columnTypeIntSmall, columnTypeLongBig) shouldBe columnTypeLongBig
       ColumnType.merge(columnTypeIntSmall, columnTypeDoubleBig) shouldBe columnTypeDoubleBig
       ColumnType.merge(columnTypeIntSmall, columnTypeStringBig) shouldBe columnTypeStringBig
       ColumnType.merge(columnTypeIntSmall, columnTypeUnknown) shouldBe columnTypeUnknown
 
+      // ColumnTypeLong
+      ColumnType.merge(columnTypeLongSmall, columnTypeBoolean) shouldBe ColumnTypeLong(ColumnLength(5))
+      ColumnType.merge(columnTypeLongSmall, columnTypeIntBig) shouldBe ColumnTypeLong(ColumnLength(2))
+      ColumnType.merge(columnTypeLongSmall, columnTypeLongBig) shouldBe columnTypeLongBig
+      ColumnType.merge(columnTypeLongSmall, columnTypeDoubleBig) shouldBe columnTypeDoubleBig
+      ColumnType.merge(columnTypeLongSmall, columnTypeStringBig) shouldBe columnTypeStringBig
+      ColumnType.merge(columnTypeLongSmall, columnTypeUnknown) shouldBe columnTypeUnknown
+
       // ColumnTypeDouble
       ColumnType.merge(columnTypeDoubleSmall, columnTypeBoolean) shouldBe ColumnTypeDouble(ColumnLength(5))
       ColumnType.merge(columnTypeDoubleSmall, columnTypeIntBig) shouldBe ColumnTypeDouble(ColumnLength(2))
+      ColumnType.merge(columnTypeDoubleSmall, columnTypeLongBig) shouldBe ColumnTypeDouble(ColumnLength(2))
       ColumnType.merge(columnTypeDoubleSmall, columnTypeDoubleBig) shouldBe columnTypeDoubleBig
       ColumnType.merge(columnTypeDoubleSmall, columnTypeStringBig) shouldBe columnTypeStringBig
       ColumnType.merge(columnTypeDoubleSmall, columnTypeUnknown) shouldBe columnTypeUnknown
@@ -49,6 +63,7 @@ class ColumnTypeSpec extends AnyFunSpec with Matchers {
       // ColumnTypeString
       ColumnType.merge(columnTypeStringSmall, columnTypeBoolean) shouldBe ColumnTypeString(ColumnLength(5))
       ColumnType.merge(columnTypeStringSmall, columnTypeIntBig) shouldBe ColumnTypeString(ColumnLength(2))
+      ColumnType.merge(columnTypeStringSmall, columnTypeLongBig) shouldBe ColumnTypeString(ColumnLength(2))
       ColumnType.merge(columnTypeStringSmall, columnTypeDoubleBig) shouldBe ColumnTypeString(ColumnLength(2))
       ColumnType.merge(columnTypeStringSmall, columnTypeStringBig) shouldBe columnTypeStringBig
       ColumnType.merge(columnTypeStringSmall, columnTypeUnknown) shouldBe columnTypeUnknown
@@ -56,6 +71,7 @@ class ColumnTypeSpec extends AnyFunSpec with Matchers {
       // ColumnTypeUnknown
       ColumnType.merge(columnTypeUnknown, columnTypeBoolean) shouldBe columnTypeUnknown
       ColumnType.merge(columnTypeUnknown, columnTypeIntBig) shouldBe columnTypeUnknown
+      ColumnType.merge(columnTypeUnknown, columnTypeLongBig) shouldBe columnTypeUnknown
       ColumnType.merge(columnTypeUnknown, columnTypeDoubleBig) shouldBe columnTypeUnknown
       ColumnType.merge(columnTypeUnknown, columnTypeStringBig) shouldBe columnTypeUnknown
       ColumnType.merge(columnTypeUnknown, columnTypeUnknown) shouldBe columnTypeUnknown
