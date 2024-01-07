f44a923f33a757978958d3291d03d84487bbc1d5
kazumatsudo <kazuma.matsudo@edandit.co.jp>
2024-01-07 15:56:16 +0900

    add: VertexUtility#toSqlSentence

--- src/main/scala/utils/vertex/VertexUtility.scala
+++ src/main/scala/utils/vertex/VertexUtility.scala
@@ -6,6 +6,9 @@ import gremlin.scala._
 
 object VertexUtility {
 
+  // TODO: Set a more detailed table name
+  private val tableName = TableName("vertex")
+
   /** convert to Database Table Information
    *
    * @param vertex [[Vertex]]
@@ -14,14 +17,15 @@ object VertexUtility {
   def toTableList(vertex: Vertex): TableList =
     TableList {
       val idColumn = Map(ColumnName("id") -> ColumnType.apply(vertex.id()))
-
       val column = vertex.valueMap.map { case (key, value) =>
         ColumnName(key) -> ColumnType.apply(value)
       }
-      val columnList = ColumnList(idColumn ++ column)
 
-      // TODO: Set a more detailed table name
-      val tableName = TableName("vertex")
-      Map(tableName -> columnList)
+      Map(tableName -> ColumnList(idColumn ++ column))
     }
+
+  def toSqlSentence(vertex: Vertex): String = {
+    val (columnList, valueList) = vertex.valueMap.unzip
+    s"INSERT INTO ${tableName.toSqlSentence} (id, ${columnList.mkString(",")}) VALUES (${vertex.id()}, ${valueList.mkString(", ")});"
+  }
 }
