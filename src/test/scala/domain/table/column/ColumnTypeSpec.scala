da334eb8103ac63c0ceb6346327f0a8f6f1b5856
kazumatsudo <kazuma.matsudo@edandit.co.jp>
2024-01-07 12:41:17 +0900

    add: ColumnType#apply

--- src/test/scala/domain/table/column/ColumnTypeSpec.scala
+++ src/test/scala/domain/table/column/ColumnTypeSpec.scala
@@ -4,6 +4,16 @@ import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 
 class ColumnTypeSpec extends AnyFunSpec with Matchers {
+  describe("apply") {
+    it("success") {
+      ColumnType.apply(false) shouldBe ColumnTypeBoolean
+      ColumnType.apply("string") shouldBe ColumnTypeString(ColumnLength(6))
+      ColumnType.apply(1) shouldBe ColumnTypeInt(ColumnLength(1))
+      ColumnType.apply(1.1) shouldBe ColumnTypeDouble(ColumnLength(3))
+      ColumnType.apply(Seq.empty) shouldBe ColumnTypeUnknown
+    }
+  }
+
   describe("merge") {
     it("success") {
       val columnTypeBoolean = ColumnTypeBoolean
