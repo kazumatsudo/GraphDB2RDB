6e557bee0e2d0cf2325d8a1c6a46a1fdab6a9a55
kazumatsudo <kazuma.matsudo@edandit.co.jp>
2024-01-07 12:04:24 +0900

    add: EdgeQuery#getEdgesList

--- src/test/scala/utils/edge/EdgeQuerySpec.scala
+++ src/test/scala/utils/edge/EdgeQuerySpec.scala
@@ -1,5 +1,6 @@
 package utils.edge
 
+import gremlin.scala.GremlinScala
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerFactory
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
@@ -10,6 +11,33 @@ class EdgeQuerySpec extends AnyFunSpec with Matchers {
       val graph = TinkerFactory.createModern().traversal()
       val edgeQuery = EdgeQuery(graph)
       edgeQuery.countAll shouldBe 6
+    }
+  }
+
+
+  describe("getVerticesList") {
+    describe("require"){
+      it ("start must be positive.") {
+        val graph = TinkerFactory.createModern().traversal()
+        val edgeQuery = EdgeQuery(graph)
+        intercept[IllegalArgumentException] {
+          edgeQuery.getEdgesList(-1, 0)
+        }
+      }
+
+      it ("count must be positive.") {
+        val graph = TinkerFactory.createModern().traversal()
+        val edgeQuery = EdgeQuery(graph)
+        intercept[IllegalArgumentException] {
+          edgeQuery.getEdgesList(-1, 0)
+        }
+      }
+    }
+
+    it ("get the number of all vertices") {
+      val graph = TinkerFactory.createModern().traversal()
+      val edgeQuery = EdgeQuery(graph)
+      edgeQuery.getEdgesList(0, 1) shouldBe Seq(GremlinScala(graph.E()).head())
     }
   }
 }
