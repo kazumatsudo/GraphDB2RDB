c5476273fcc3623e50ca436eb722a470b611ee27
kazumatsudo <kazuma.matsudo@edandit.co.jp>
2024-01-07 16:20:05 +0900

    add: FileUtility#outputSql

---
+++ src/main/scala/utils/FileUtility.scala
@@ -0,0 +1,20 @@
+package utils
+
+import java.io.{File, FileOutputStream, OutputStreamWriter}
+
+object FileUtility {
+
+  def outputSql(filename: String, sqlSentence: String): Unit = {
+    val directory = new File("sql")
+
+    if (!directory.exists()) {
+      directory.mkdirs()
+    }
+
+    val fileOutPutStream = new FileOutputStream(s"${directory.getName}/$filename.sql")
+    val writer = new OutputStreamWriter(fileOutPutStream)
+
+    writer.write(sqlSentence)
+    writer.close()
+  }
+}
