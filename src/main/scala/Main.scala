27c78c0fa41874cb619c87b82f4710e852bc18fd
kazumatsudo <kazuma.matsudo@edandit.co.jp>
2024-01-14 11:02:11 +0900

    refactor: use ByExhaustiveSearch

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,26 +1,12 @@
 import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.StrictLogging
-import domain.table.ddl.TableList
-import domain.table.dml.RecordList
-import infrastructure.{EdgeQuery, VertexQuery}
 import org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource.traversal
+import usecase.ByExhaustiveSearch
 import utils.FileUtility
 
 import scala.util.Using
-import scala.util.control.NonFatal
 
 object Main extends StrictLogging {
-
-  private def executeWithExceptionHandling(function: => Unit): Boolean = {
-    try {
-      function
-      true
-    } catch {
-      case NonFatal(e) =>
-        logger.error(s"An exception occurred: ${e.getMessage}", e)
-        false
-    }
-  }
 
   private def displayOperationResult(
       processName: String,
@@ -44,84 +30,36 @@ object Main extends StrictLogging {
         config.getString("graphdb_remote_graph_properties")
       )
     ) { g =>
-      val vertexQuery = VertexQuery(g)
-      val totalVertexCount = vertexQuery.countAll.toInt
+      val usecase = ByExhaustiveSearch(g)
+      val (vertexResult, edgeResult) = usecase.execute
 
-      val edgeQuery = EdgeQuery(g)
-      val totalEdgeCount = edgeQuery.countAll.toInt
+      vertexResult match {
+        case Some((ddl, dml)) =>
+          FileUtility.outputSql(
+            config.getString("sql_ddl_vertex"),
+            ddl.toSqlSentence
+          )
+          FileUtility.outputSql(
+            config.getString("sql_dml_vertex"),
+            dml.toSqlSentence
+          )
+        case None =>
+      }
+      displayOperationResult("generate vertex SQL", vertexResult.nonEmpty)
 
-      // 1. generate vertex SQL
-      val generateVertexSqlResult = executeWithExceptionHandling({
-        val (ddl, dml) = (0 to totalVertexCount)
-          .flatMap { start =>
-            vertexQuery
-              .getList(start, 1)
-              .headOption
-              .map(vertex =>
-                (
-                  vertex.toDdl,
-                  vertex.toDml
-                )
-              )
-          }
-          .reduce[(TableList, RecordList)] {
-            case (
-                  (tableListAccumlator, dmlAccumlator),
-                  (tableListCurrentValue, dmlCurrentValue)
-                ) =>
-              (
-                tableListAccumlator.merge(tableListCurrentValue),
-                dmlAccumlator.merge(dmlCurrentValue)
-              )
-          }
-        FileUtility.outputSql(
-          config.getString("sql_ddl_vertex"),
-          ddl.toSqlSentence
-        )
-        FileUtility.outputSql(
-          config.getString("sql_dml_vertex"),
-          dml.toSqlSentence
-        )
-      })
-
-      // 2. generate edge DDL
-      val generateEdgeSqlResult = executeWithExceptionHandling({
-        val (ddl, dml) = (0 to totalEdgeCount)
-          .flatMap { start =>
-            edgeQuery
-              .getList(start, 1)
-              .headOption
-              .map(edge =>
-                (
-                  edge.toDdl,
-                  edge.toDml
-                )
-              )
-          }
-          .reduce[(TableList, RecordList)] {
-            case (
-                  (tableListAccumlator, dmlAccumlator),
-                  (tableListCurrentValue, dmlCurrentValue)
-                ) =>
-              (
-                tableListAccumlator.merge(tableListCurrentValue),
-                dmlAccumlator.merge(dmlCurrentValue)
-              )
-          }
-
-        FileUtility.outputSql(
-          config.getString("sql_ddl_edge"),
-          ddl.toSqlSentence
-        )
-        FileUtility.outputSql(
-          config.getString("sql_dml_edge"),
-          dml.toSqlSentence
-        )
-      })
-
-      logger.info(s"generate SQL process is finished.")
-      displayOperationResult("generate vertex SQL", generateVertexSqlResult)
-      displayOperationResult("generate edge SQL  ", generateEdgeSqlResult)
+      edgeResult match {
+        case Some((ddl, dml)) =>
+          FileUtility.outputSql(
+            config.getString("sql_ddl_edge"),
+            ddl.toSqlSentence
+          )
+          FileUtility.outputSql(
+            config.getString("sql_dml_edge"),
+            dml.toSqlSentence
+          )
+        case None =>
+      }
+      displayOperationResult("generate edge SQL  ", edgeResult.nonEmpty)
     }
   }
 }
