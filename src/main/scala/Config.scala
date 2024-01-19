966b81262874e63eae910c4417bf9f89bfa67ed9
kazumatsudo <kazuma.matsudo@edandit.co.jp>
2024-01-19 23:43:40 +0900

    wip: add Config

---
+++ src/main/scala/Config.scala
@@ -0,0 +1,65 @@
+import com.typesafe.config.ConfigFactory
+
+final case class AnalysysMethod(
+    private val value: String,
+    private val using_specific_key_list_filepath: String
+)
+final case class GraphDb(private val remoteGraphProperties: String)
+final case class Sql(
+    private val ddl_edge: String,
+    private val ddl_vertex: String,
+    private val dml_edge: String,
+    private val dml_vertex: String,
+    private val output_directory: String
+)
+final case class TableName(
+    private val edge: String,
+    private val vertex: String
+)
+final case class ColumnName(
+    private val edgeId: String,
+    private val edgeInVId: String,
+    private val edgeOutVId: String,
+    private val vertexId: String,
+    private val prefixProperty: String
+)
+final case class Config(
+    private val analysysMethod: AnalysysMethod,
+    private val graphDb: GraphDb,
+    private val sql: Sql,
+    private val tableName: TableName,
+    private val columnName: ColumnName
+)
+
+object Config {
+
+  val default: Config = {
+    val config = ConfigFactory.load()
+
+    val analysysMethod = AnalysysMethod(
+      config.getString("analysis_method"),
+      config.getString("analysis_method_using_specific_key_list_filepath")
+    )
+    val graphDb = GraphDb(config.getString("graphdb_remote_graph_properties"))
+    val sql = Sql(
+      config.getString("sql_ddl_edge"),
+      config.getString("sql_ddl_vertex"),
+      config.getString("sql_dml_edge"),
+      config.getString("sql_dml_vertex"),
+      config.getString("sql_output_directory")
+    )
+    val tableName = TableName(
+      config.getString("table_name_edge"),
+      config.getString("table_name_vertex")
+    )
+    val columnName = ColumnName(
+      config.getString("column_name_edge_id"),
+      config.getString("column_name_edge_in_v_id"),
+      config.getString("column_name_edge_out_v_id"),
+      config.getString("column_name_vertex_id"),
+      config.getString("column_name_prefix_property")
+    )
+
+    Config(analysysMethod, graphDb, sql, tableName, columnName)
+  }
+}
---
+++ src/test/scala/ConfigSpec.scala
@@ -0,0 +1,19 @@
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+class ConfigSpec extends AnyFunSpec with Matchers {
+  describe("default") {
+    it("success") {
+      Config.default shouldBe Config(
+        AnalysysMethod(
+          "by_exhaustive_search",
+          "src/main/resources/using_key_list_file.json"
+        ),
+        GraphDb("conf/remote-graph.properties"),
+        Sql("ddl_edge", "ddl_vertex", "dml_edge", "dml_vertex", "sql"),
+        TableName("edge", "vertex"),
+        ColumnName("id", "id_in_v", "id_out_v", "id", "property_")
+      )
+    }
+  }
+}
