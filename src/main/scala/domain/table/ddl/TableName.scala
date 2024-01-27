10c63f095012921b92d274262a8fa1868edd5fbd
kazumatsudo <kazuma.matsudo@edandit.co.jp>
2024-01-27 23:26:52 +0900

    fix: substring table name length for foreign key

--- src/main/scala/domain/table/ddl/TableName.scala
+++ src/main/scala/domain/table/ddl/TableName.scala
@@ -1,7 +1,9 @@
 package domain.table.ddl
 
 case class TableName(private val value: String) extends AnyVal {
-  private def maxLength = 64
+
+  // for foreign_key, substring 7 characters ('_ibfk_1')
+  private def maxLength = 64 - 7
 
   def toSqlSentence: String = if (value.length > maxLength) {
     value.substring(0, maxLength)
--- src/test/scala/domain/table/ddl/TableNameSpec.scala
+++ src/test/scala/domain/table/ddl/TableNameSpec.scala
@@ -5,16 +5,16 @@ import org.scalatest.matchers.should.Matchers
 
 class TableNameSpec extends AsyncFunSpec with Matchers {
   describe("toSqlSentence") {
-    it("if the length is less than 64, return as is") {
-      val tableName = "a" * 64
+    it("if the length is less than (64 - 7), return as is") {
+      val tableName = "a" * (64 - 7)
       TableName(tableName).toSqlSentence shouldBe tableName
     }
 
     it(
-      "if the length is 64 or more, return the truncated one after 65 truncated"
+      "if the length is (64 - 7) or more, return the truncated one after (64 - 7) + 1 truncated"
     ) {
-      val tableName = "a" * 65
-      TableName(tableName).toSqlSentence shouldBe tableName.substring(0, 64)
+      val tableName = "a" * (64 - 7) + 1
+      TableName(tableName).toSqlSentence shouldBe tableName.substring(0, 64 - 7)
     }
   }
 }
