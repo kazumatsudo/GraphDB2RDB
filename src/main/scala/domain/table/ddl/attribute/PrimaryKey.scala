df6096f303891739d440659d9ec1c0ef8b3033b1
kazumatsudo <kazuma.matsudo@edandit.co.jp>
2024-01-28 08:48:06 +0900

    add: PrimaryKey

---
+++ src/main/scala/domain/table/ddl/attribute/PrimaryKey.scala
@@ -0,0 +1,24 @@
+package domain.table.ddl.attribute
+
+import domain.table.ddl.column.ColumnName
+
+case class PrimaryKey(private val value: Set[ColumnName]) extends AnyVal {
+
+  /** merges PrimaryKey in two primary key list into one
+    *
+    * @param target
+    *   target PrimaryKey
+    * @return
+    *   merged primary key list
+    */
+  def merge(target: PrimaryKey): PrimaryKey = if (value == target.value) {
+    this
+  } else {
+    throw new IllegalArgumentException(
+      s"primary key must be unique. detected values: $value and ${target.value}"
+    )
+  }
+
+  def toSqlSentence: String =
+    s"PRIMARY KEY (${value.toSeq.sortBy(_.toSqlSentence).map(_.toSqlSentence).mkString(", ")})"
+}
