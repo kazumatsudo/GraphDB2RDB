15282423c9ff8a6c7f3a179ed5cc7fae0db963b2
kazumatsudo <kazuma.matsudo@edandit.co.jp>
2024-01-28 07:29:43 +0900

    fix: decrease TableName#maxLength

--- src/main/scala/domain/table/ddl/TableName.scala
+++ src/main/scala/domain/table/ddl/TableName.scala
@@ -2,8 +2,9 @@ package domain.table.ddl
 
 case class TableName(private val value: String) extends AnyVal {
 
+  // in MySQL, the length must be less than 63 (2^6 - 1)
   // for foreign_key, substring 7 characters ('_ibfk_1')
-  private def maxLength = 64 - 7
+  private def maxLength = 63 - 7
 
   def toSqlSentence: String = if (value.length > maxLength) {
     value.substring(0, maxLength)
--- src/test/scala/domain/table/ddl/TableNameSpec.scala
+++ src/test/scala/domain/table/ddl/TableNameSpec.scala
@@ -5,16 +5,16 @@ import org.scalatest.matchers.should.Matchers
 
 class TableNameSpec extends AsyncFunSpec with Matchers {
   describe("toSqlSentence") {
-    it("if the length is less than (64 - 7), return as is") {
-      val tableName = "a" * (64 - 7)
+    it("if the length is less than (63 - 7), return as is") {
+      val tableName = "a" * (63 - 7)
       TableName(tableName).toSqlSentence shouldBe tableName
     }
 
     it(
-      "if the length is (64 - 7) or more, return the truncated one after (64 - 7) + 1 truncated"
+      "if the length is (63 - 7) or more, return the truncated one after (63 - 7) + 1 truncated"
     ) {
-      val tableName = "a" * (64 - 7) + 1
-      TableName(tableName).toSqlSentence shouldBe tableName.substring(0, 64 - 7)
+      val tableName = "a" * (63 - 7) + 1
+      TableName(tableName).toSqlSentence shouldBe tableName.substring(0, 63 - 7)
     }
   }
 }
